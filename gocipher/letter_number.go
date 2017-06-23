7c1fb5edf960d35317b5dfa926624ff7f62e1909
Andrew Archibald <andrew@robarchibald.com>
2017-06-23 16:55:53 -0700

    Add letter to number doc comments

--- gocipher/letter_number.go
+++ gocipher/letter_number.go
@@ -1,5 +1,7 @@
 package gocipher
 
+// LetterNumberEncrypt - Converts letters to the corresponding number.
+// e.g. "ABC...XYZ" becomes []int{1, 2, 3 ... 24, 25, 26}
 func LetterNumberEncrypt(text string) []int {
 	runes := []rune(text)
 	numbers := make([]int, len(runes))
@@ -13,6 +15,8 @@ func LetterNumberEncrypt(text string) []int {
 	return numbers
 }
 
+// LetterNumberDecrypt - Converts numbers to the corresponding letter.
+// e.g. []int{1, 2, 3 ... 24, 25, 26} becomes "ABC...XYZ"
 func LetterNumberDecrypt(numbers []int) string {
 	runes := make([]rune, len(numbers))
 	for i, number := range numbers {
