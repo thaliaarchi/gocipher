42cbc00be6f92ac36328883a84b21580d32dc23c
Andrew Archibald <andrew@robarchibald.com>
2017-06-26 22:23:04 -0700

    Add visual test for n-gram file reading

--- gocipher/ngrams_read.go
+++ gocipher/ngrams_read.go
@@ -8,15 +8,39 @@ import (
 	"strings"
 )
 
-type NgramEntry struct {
-	ngram string
+type Language struct {
+	name  string
+	files []string
+}
+
+var languages = []Language{
+	{"danish", []string{"monograms", "bigrams", "trigrams", "quadgrams"}},
+	{"english", []string{"monograms", "bigrams", "trigrams", "quadgrams", "quintgrams", "words"}},
+	{"finnish", []string{"monograms", "bigrams", "trigrams", "quadgrams"}},
+	{"french", []string{"monograms", "bigrams", "trigrams", "quadgrams"}},
+	{"german", []string{"monograms", "bigrams", "trigrams", "quadgrams"}},
+	{"icelandic", []string{"monograms", "bigrams", "trigrams", "quadgrams"}},
+	{"polish", []string{"monograms", "bigrams", "trigrams", "quadgrams"}},
+	{"russian", []string{"monograms", "bigrams", "trigrams", "quadgrams"}},
+	{"spanish", []string{"monograms", "bigrams", "trigrams", "quadgrams"}},
+	{"swedish", []string{"monograms", "bigrams", "trigrams", "quadgrams", "words"}}}
+
+/*type NgramSet struct {
+	fileName string
+	ngrams   []Ngram
+	ngramMap map[string]Ngram
+	n        int
+}*/
+
+type Ngram struct {
+	chars string
 	count int
 	freq  float64
 }
 
 // readNgramFile reads and parses all n-grams from a file.
 // See: https://stackoverflow.com/a/23667119/3238709.
-func readNgramFile(fileName string) ([]*NgramEntry, map[string]*NgramEntry) {
+func readNgramFile(fileName string) ([]*Ngram, map[string]*Ngram, int) {
 	if fileName == "" {
 		fmt.Println("Filename must not be empty")
 		os.Exit(2)
@@ -25,8 +49,8 @@ func readNgramFile(fileName string) ([]*NgramEntry, map[string]*NgramEntry) {
 	if err != nil {
 		panic(fmt.Sprintf("error opening file %q: %v", fileName, err))
 	}
-	var ngrams []*NgramEntry
-	var ngramMap = make(map[string]*NgramEntry)
+	var ngrams []*Ngram
+	var ngramMap = make(map[string]*Ngram)
 	countTotal := 0
 	scanner := bufio.NewScanner(file)
 	for i := 0; scanner.Scan(); i++ {
@@ -40,16 +64,13 @@ func readNgramFile(fileName string) ([]*NgramEntry, map[string]*NgramEntry) {
 		if err != nil {
 			fmt.Println("N-gram count cannot be parsed to integer:", text, err)
 		}
-		entry := &NgramEntry{ngram: text[:split], count: int(count)}
+		entry := &Ngram{chars: text[:split], count: int(count)}
 		ngrams = append(ngrams, entry)
-		ngramMap[entry.ngram] = entry
+		ngramMap[entry.chars] = entry
 		countTotal += int(count)
-		fmt.Println(text, entry)
 	}
 	for i := range ngrams {
 		ngrams[i].freq = float64(ngrams[i].count) / float64(countTotal)
-		fmt.Println(ngrams[i])
 	}
-	fmt.Println(ngrams[0], ngramMap["E"])
-	return ngrams, ngramMap
+	return ngrams, ngramMap, countTotal
 }
--- gocipher/ngrams_read_test.go
+++ gocipher/ngrams_read_test.go
@@ -1,7 +1,24 @@
 package gocipher
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestReadNgrams(t *testing.T) {
-	readNgramFile("ngrams/english_monograms.txt")
+	type fileData struct {
+		lang  string
+		file  string
+		count int
+	}
+	counts := []fileData{}
+	for _, lang := range languages {
+		for _, file := range lang.files {
+			_, _, count := readNgramFile("ngrams/" + lang.name + "_" + file + ".txt")
+			data := fileData{lang.name, file, count}
+			counts = append(counts, data)
+			fmt.Println(data)
+		}
+	}
+	fmt.Println(counts)
 }
