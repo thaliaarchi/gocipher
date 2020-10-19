ea171a4a9d74b4f264faa16f8b855c49745be75d
Andrew Archibald <andrew@aarchibald.com>
2020-10-19 10:12:26 -0600

    Revise RSA short plaintext notes

--- rsa/attack.go
+++ rsa/attack.go
@@ -1,39 +1,39 @@
 package rsa
 
-import (
-	"math/big"
-)
+import "math/big"
 
 // AttackShortPlaintext exploits short plaintexts to compute the key.
 func (k *PublicKey) AttackShortPlaintext(c *big.Int, bits int) (m *big.Int) {
+	// Described in Introduction to Cryptography with Coding Theory by
+	// Trappe and Washington, section 6.2.2.
+
 	bm := make(bigMap)
 	tmp := new(big.Int)
 
 	// Find x and y such that c*(x^-e) = y^e (mod n)
-	i := big.NewInt(1)
 	var x, y *big.Int
-	for i.BitLen() <= bits {
-		ye := new(big.Int).Exp(i, k.E, k.N) // i^e      (mod n)
-		tmp.ModInverse(ye, k.N)             // i^-e     (mod n)
-		xe := new(big.Int).Mul(c, tmp)      // c*(i^-e) (mod n)
-		xe.Mod(xe, k.N)
+	for i := one; i.BitLen() <= bits; i = new(big.Int).Add(i, one) {
+		yi := new(big.Int).Exp(i, k.E, k.N) // i^e      (mod n)
+		tmp.ModInverse(yi, k.N)             // i^-e     (mod n)
+		xi := new(big.Int).Mul(c, tmp)      // c*(i^-e) (mod n)
+		xi.Mod(xi, k.N)
 
 		// Check for match
-		if v, ok := bm.GetOrPut(xe, i); ok {
+		if v, ok := bm.GetOrPut(xi, i); ok {
 			x, y = i, v
 			break
 		}
-		if v, ok := bm.GetOrPut(ye, i); ok {
+		if v, ok := bm.GetOrPut(yi, i); ok {
 			x, y = v, i
 			break
 		}
-		i = new(big.Int).Add(i, one)
 	}
 
-	// We now have c = (x*y)^e (mod n), thus m = x*y (mod n)
-	xy := new(big.Int).Mul(x, y)
-	xy.Mod(xy, k.N)
-	return xy
+	// c*(x^-e) = y^e (mod n)
+	// c = (x*y)^e    (mod n)
+	// m = x*y        (mod n)
+	m = tmp.Mod(tmp.Mul(x, y), k.N)
+	return m
 }
 
 // bigMap is a hash table for *big.Int keys. Keys are not copied and
